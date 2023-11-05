c26f5326d60e6aa254737614a1d5c006891947f7
Hessam Mahdiabadi <67460597+iamHEssam@users.noreply.github.com>
2023-11-06 01:00:53 +0330

    Add SafeArea Extension, Pin to Superview, and CollectionView Cell Registration Extension
    - Added a SafeArea extension for safe area handling.
    - Added a Pin to Superview extension for view positioning.
    - Added an extension for easier registration and dequeuing of cells in collection views.

--- Data/Package.swift
+++ Data/Package.swift
@@ -23,7 +23,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.        
         .target(
             name: "Data",
-            dependencies: ["Alamofire", "Domain"]),
+            dependencies: ["Alamofire", "Domain"],
+            resources: [.process("Resources")]),
         .testTarget(
             name: "DataTests",
             dependencies: ["Data"]),
